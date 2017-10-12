46e35bfa6fffeb47c70379538a49334e074de4d9
amhursh <amhursh@gmail.com>
2017-10-11 23:16:54 -0600

    Refactor to account for token expiration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,18 @@
 class User < ApplicationRecord
 
   def self.update_or_create(auth)
-    where(uid: auth[:uid]).first_or_create do |user|
-      user.uid = auth[:uid]
-      user.provider = auth[:provider]
-      user.nickname = auth[:info][:nickname]
-      user.name = auth[:info][:name]
-      user.email = auth[:info][:email]
-      user.image = auth[:info][:image]
-      user.token = auth[:credentials][:token]
-    end
+    user = find_by(uid: auth[:uid]) || User.new
+    user.attributes = {
+      uid: auth[:uid],
+      provider: auth[:provider],
+      nickname: auth[:info][:nickname],
+      name: auth[:info][:name],
+      email: auth[:info][:email],
+      image: auth[:info][:image],
+      token: auth[:credentials][:token]
+    }
+    user.save
+    user
   end
 
 end
